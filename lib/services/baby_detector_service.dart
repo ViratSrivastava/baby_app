1e678c58bc029f6a4f6ad7556ceb7a33fdb94692
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-12-01 12:25:31 +0530

    Update baby_detector_service.dart

---
+++ lib/services/baby_detector_service.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import '../utils/tflite_inference.dart';
+
+class BabyDetectorService {
+  final TFLiteInference _inference = TFLiteInference();
+  final double confidenceThreshold = 0.65;
+
+  bool detectBabyFace(Uint8List image) {
+    var predictions = _inference.runModel(image);
+    for (var pred in predictions) {
+      if (pred['class'] == 'babynormal' && pred['confidence'] > confidenceThreshold) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
