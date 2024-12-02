4782d13a689fd9d743479d9b5ea0962d81ac74a2
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2024-12-02 23:28:23 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,43 @@ class BabyLockApp extends StatelessWidget {
     );
   }
 }
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      home: Scaffold(
+
+        appBar: AppBar(
+
+          title: const Text('Counter App'),
+
+        ),
+
+        body: const Center(
+
+          child: Text('0'),
+
+        ),
+
+        floatingActionButton: FloatingActionButton(
+
+          onPressed: () {},
+
+          child: const Icon(Icons.add),
+
+        ),
+
+      ),
+
+    );
+
+  }
+
+}
